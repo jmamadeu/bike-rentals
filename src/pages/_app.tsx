4561a64f8d1a7038be4ee07e385090c4d8bd57a2
João Amadeu <jmamadeu2000@gmail.com>
2021-11-07 01:47:58 +0100

    chore: add auth pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
+import { ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 import type { AppProps } from 'next/app';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from '../services/react-query';
+import { theme } from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
