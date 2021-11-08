9175f2382fccc44868f1c08ad73aa4286011b5c9
jmamadeu <jmamadeu2000@gmail.com>
2021-11-08 03:40:54 +0100

    chore: add user form

---
+++ src/components/save-user-form.tsx
@@ -0,0 +1,136 @@
+import { LoadingButton } from '@mui/lab';
+import {
+  Alert,
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography
+} from '@mui/material';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+
+enum RolesEnum {
+  user = 'user',
+  manager = 'manager',
+}
+
+export type ISaveUserFormInputs = {
+  name: string;
+  email: string;
+  password: string;
+  roles: RolesEnum;
+};
+
+export type SaveUserFormProperties = {
+  defaultData?: ISaveUserFormInputs;
+  onSubmit: (user: ISaveUserFormInputs) => void;
+  error?: string;
+  title: string;
+  isLoading: boolean;
+};
+
+export const SaveUserForm = ({
+  error,
+  title,
+  isLoading,
+  defaultData,
+  onSubmit,
+}: SaveUserFormProperties) => {
+  const { register, handleSubmit, control } = useForm<ISaveUserFormInputs>({
+    defaultValues: defaultData,
+  });
+
+  const getFormData: SubmitHandler<ISaveUserFormInputs> = user => onSubmit(user);
+
+  return (
+    <section>
+      <Box
+        component="form"
+        onSubmit={handleSubmit(getFormData)}
+        style={{ width: '50ch' }}
+        sx={{
+          '& .MuiPaper-root': { m: 1, width: '100%' },
+          '& .MuiFormControl-root': { m: 1 },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Typography variant="h4" gutterBottom component="div">
+          {title}
+        </Typography>
+
+        {error && (
+          <Alert severity="error" color="error">
+            {error}
+          </Alert>
+        )}
+
+        <Box style={{ flexDirection: 'column', display: 'flex' }}>
+          <TextField
+            {...register('name', { required: true })}
+            required
+            id="name"
+            type="text"
+            label="Name"
+            fullWidth
+          />
+          <TextField
+            {...register('email', { required: true })}
+            required
+            id="email"
+            type="email"
+            label="Email"
+            fullWidth
+          />
+          <TextField
+            {...register('password', { required: true })}
+            required
+            type="password"
+            id="password"
+            label="Password"
+            fullWidth
+          />
+
+          <Controller
+            control={control}
+            name="roles"
+            render={({ field }) => (
+              <FormControl fullWidth>
+                <InputLabel id="roles">Role</InputLabel>
+                <Select
+                  {...field}
+                  labelId="roles"
+                  id="roles"
+                  label="Role"
+                  {...register('roles', { required: true })}
+                >
+                  <MenuItem value="user">User</MenuItem>
+                  <MenuItem value="manager">Manager</MenuItem>
+                </Select>
+              </FormControl>
+            )}
+          />
+        </Box>
+        <Box style={{ margin: '8px' }}>
+          <LoadingButton
+            sx={{
+              marginBottom: 1,
+              '.MuiLoadingButton-startIconLoadingStart': {
+                marginRight: 1,
+              },
+            }}
+            loading={isLoading}
+            variant="outlined"
+            type="submit"
+            loadingIndicator="Saving"
+          >
+            Save
+          </LoadingButton>
+          <br />
+        </Box>
+      </Box>
+    </section>
+  );
+};
